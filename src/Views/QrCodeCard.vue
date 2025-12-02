93ec7f91915830601c7d73ec319e0eb2ed4c0c55
Fernando Léon <ferzleon51@gmail.com>
2025-12-02 12:12:41 -0600

    feat: Agregar componente QrCodeCard y su estilo asociado

---
+++ src/Views/QrCodeCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="body-wrapper">
+    <QrDetails
+      :imageSrc="cardData.imageSrc"
+      :imageAlt="cardData.imageAlt"
+      :title="cardData.title"
+      :paragraph="cardData.paragraph"
+    />
+    <Attribution />
+  </main>
+</template>
+
+<script setup>
+import Attribution from '@/components/Attribution.vue';
+import QrDetails from '@/components/qrCode/QrDetails.vue';
+import { ref } from "vue";
+
+const cardData = ref({
+  imageSrc: '/qrcode/image-qr-code.png',
+  imageAlt: 'QR Code Logo',
+  title: 'Improve your front-end skills by building projects',
+  paragraph: 'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level'
+});
+</script>
+
+<style scoped>
+.body-wrapper {
+  @apply h-screen bg-LightGray flex flex-col items-center justify-center font-Outfit;
+}
+</style>
---
+++ src/components/qrCode/QrDetails.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  imageSrc: String,
+  imageAlt: String,
+  title: String,
+  paragraph: String
+});
+</script>
+
+<template>
+  <div class="qrcard">
+    <img
+      class="qrlogo"
+      :src="imageSrc"
+      :alt="imageAlt"
+    />
+    <h1 class="title">
+      {{ title }}
+    </h1>
+    <p class="paragraph">
+      {{ paragraph }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+/* Las clases con @apply se aplican aquí usando el estilo SCALED (puedes usar div en lugar de main) */
+.qrcard {
+  /* Añadimos padding y margen al texto para separación (p-4 es del CSS original) */
+  @apply text-center m-4 p-4 rounded-2xl bg-white max-w-[296px]; 
+}
+
+.qrlogo {
+  /* Ajustamos el margen para que no esté pegada al borde superior */
+  @apply rounded-lg mt-0; 
+}
+
+.title {
+  /* Aquí aplicamos tus custom font sizes: text-title */
+  @apply mt-4 px-2 text-title-custom text-DarkBlue;
+}
+
+.paragraph {
+  /* Aquí aplicamos tus custom font sizes: text-paragraph */
+  @apply mt-4 px-2 text-paragraph-custom text-GrayishBlue; 
+}
+</style>
